11fc2f4e572358cd5e1056399c11c1969bc3dd4e
Nicholas Bottomley <bottomleynj@gmail.com>
2026-03-29 23:03:13 -0400

    Viterbi up to traceback step

--- project_hmm/HMM_class_utils.py
+++ project_hmm/HMM_class_utils.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 from numbers import Number
 from collections.abc import Iterable
+from math import log
 
 class State:
     """Hidden state for HMM"""
-    def __init__(self, name:str, emissions: list, probabilities: list[Number]):
+    def __init__(self, name:str, emissions: list, probabilities: list[Number], transitions:dict[str:float]):
         self.name = name
         self.emissions = set(emissions) # Create a set of emission labels for faster future checks
         self.emission_probs = dict(zip(emissions, probabilities))  # Create state emission: probabilities pairs from input
+        self.transitions = transitions
         self.total_emission_prob = sum(probabilities)  # Save sum of emission probabilities
         if self.total_emission_prob != 1:  # Confirm emission probabilities sum to 1, else raise error
             raise ValueError("Emission probabilities do not sum to 1")
@@ -41,9 +43,10 @@ class State:
     
 class HMM():
     def __init__(self, name: str, transitions: np.matrix, betas: dict[State:float], emissions: set = set(), states: list[State] = []):
-        self.name = name
-        self.states = states
-        self.transitions = pd.DataFrame(transitions, index=states, columns=states)
+        self.name = name  # Name for identification
+        self.states = states  # List of state objects in HMM
+        self.t_mat = self.build_transition_mat_from_states()  # Transition matrix of states -> states in HMM
+        self.e_mat = self.build_emission_mat_from_states()  # Emission matrix of states -> emissions in HMM
         self.emissions = set(emissions)
         self.betas = betas
 
@@ -51,8 +54,26 @@ class HMM():
         return(f"{self.name}\n"
                f"{self.emissions}\n"
                f"{self.states}\n"
-               f"{self.transitions}\n"
+               f"{self.t_mat}\n"
                f"{self.betas}")
+    
+    def build_transition_mat_from_states(self):
+        t_mat = [[] for state in self.states]  # Initialize empty list of lists for transition matrix
+
+        # Fill transition matrix list with transition probabilities from states in HMM.states
+        for row, state in enumerate(self.states):
+            for trans in self.states:
+                t_mat[row].append(state.transitions[trans.name])
+
+        return np.array(t_mat)
+    
+    def build_emission_mat_from_states(self):
+        emat = [[] for state in self.states]  # Initialize empty list of lists for emission matrix
+
+        # Fill emission matrix list with emission probabilities from states in HMM.states
+        pass
+
+
 
     def add_state(self, name:str = None, emissions: list = [], probabilities: list = [], state:State = None):
         """
@@ -88,19 +109,39 @@ class HMM():
                 if not emission in state.emissions:
                     state.add_emission(emission=emission, probability=0)
     
-    def viterbi(self, beta:list, observations: Iterable) -> list[State]:
+    def viterbi(self, observations: Iterable) -> list[State]:
         """
         Calculate most likely state at each observation
 
         Args:
-            beta (list): list of starting probabilities for each state
             observations (Iterable): iterable of observations
 
         Returns:
             list: list of which state is most likely at each observation
         """
+        # Initialize empty arrays to hold path probabilities and traceback
+        vit = np.zeros((len(self.states), len(observations)))
+        traceback = np.zeros((len(self.states), len(observations)))
 
-        np.zeros((len(self.states), len(observations)))
+        # Set 0 index of trace array rows to index not in states list to recognize as stop signal
+        for row in traceback:
+            row[0] = len(self.states)
+
+        # Set 0 index of probability array rows to beta probability * emission probability
+        for state, row in enumerate(vit):
+            row[0]=log(self.betas[self.states[state]]) + log(self.states[state].emission_probs[observations[0]])
+
+        # Populate each row at each position with max probability of cumulative prob * transition prob * emission prob
+        for obs in range(1, len(observations)):
+            for state, row in enumerate(vit):
+                options = [state_row[obs-1] + log(self.t_mat[trans_state][state]) + log(self.states[state].emission_probs[observations[obs]])
+                           for trans_state, state_row in enumerate(vit)] 
+                row[obs] = max(options)  # Take highest probability of possible paths to this emission
+                traceback[state][obs] = options.index(row[obs]) # Save direction path extended from as index in states list
+        print(traceback)
+        return vit
+
+
 
 
                 
@@ -109,16 +150,17 @@ class HMM():
 
 
 if __name__ == "__main__":
+    observations = "AAABBCCABCBCABCB"
+
     my_name = "my_state"
     my_emissions = ["A", "B", "C"]
     my_probs = [0.3, 0.2, 0.5]
-    my_state = State(name="my_state", emissions=my_emissions, probabilities=my_probs)
-    my_state2 = State(name = "my_state2", emissions=["A", "B", "C"], probabilities=[0.2, 0.7, 0.1])
+    my_state = State(name="my_state", emissions=my_emissions, probabilities=my_probs, transitions={"my_state":0.7, "my_state2":0.3})
+    my_state2 = State(name = "my_state2", emissions=["A", "B", "C"], probabilities=[0.2, 0.7, 0.1], transitions={"my_state2":0.9, "my_state":0.1})
 
     transitions = np.matrix([[0.9, 0.1],[0.7,0.3]])
     my_HMM = HMM(name="My_HMM", transitions=transitions, betas={my_state:0.5, my_state2:0.5}, emissions={"A", "B", "C"}, states=[my_state, my_state2])
     print(my_HMM)
 
-    print(my_HMM.transitions[my_state][my_state2]) # column -> row
-
+    print(my_HMM.viterbi(observations=observations))
 
